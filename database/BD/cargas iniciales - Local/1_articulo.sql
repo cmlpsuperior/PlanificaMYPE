0dee1ca299c70d256c2b999ed8d9a08084b9b266
Henry A <henryespinozat@gmail.com>
2018-02-12 00:47:41 -0500

    nuevas bases de datos

--- database/BD/cargas iniciales - Local/1_articulo.sql
+++ database/BD/cargas iniciales - Local/1_articulo.sql
@@ -1,11 +1,11 @@
 
 
 INSERT INTO `articulo` (`idArticulo`, `nombre`, `precioBase`, `stock`, `volumen`, `tiempoHorasAbastecer`, `combinable`, `minimoDivisible`, `activo`, `idMarca`, `idTipoCarga`, `idUnidadMedida`) VALUES
-(1, 'Cemento sol tipo 1', 28.5, 449, 0.01, NULL, 1, 1, 1, 2, 1, 3),
-(2, 'Arena gruesa', 55.5, 7, 1, NULL, 0, 0.5, 1, 6, 1, 2),
-(3, 'Arena fina', 50, 6, 1, NULL, 0, 0.5, 1, 11, 1, 2),
-(4, 'Ladrillo pandereta', 0.5, 6000, 0.001, NULL, 1, 1, 1, 8, 1, 5),
-(5, 'Fierro de 1/2"', 28.5, 500, 0.01, NULL, 1, 1, 1, 9, 3, 4),
-(6, 'Fierro de 5/8"', 54, 100, 0.01, NULL, 1, 1, 1, 9, 3, 4),
-(7, 'Clavo 2"', 4.5, 496, 0, NULL, 1, 1, 1, 9, 2, 1),
-(8, 'Yeso', 0.5, 499, 0, NULL, 1, 1, 1, 11, 2, 1);
+(1, 'Cemento sol tipo 1', 28.5, 5000, 0.01, NULL, 1, 1, 1, 2, 1, 3),
+(2, 'Arena gruesa', 55.5, 5000, 1, NULL, 0, 0.5, 1, 6, 1, 2),
+(3, 'Arena fina', 50, 5000, 1, NULL, 0, 0.5, 1, 11, 1, 2),
+(4, 'Ladrillo pandereta', 0.5, 16000, 0.001, NULL, 1, 1, 1, 8, 1, 5),
+(5, 'Fierro de 1/2"', 28.5, 5000, 0.01, NULL, 1, 1, 1, 9, 3, 4),
+(6, 'Fierro de 5/8"', 54, 11000, 0.01, NULL, 1, 1, 1, 9, 3, 4),
+(7, 'Clavo 2"', 4.5, 15000, 0, NULL, 1, 1, 1, 9, 2, 1),
+(8, 'Yeso', 0.5, 7000, 0, NULL, 1, 1, 1, 11, 2, 1);
